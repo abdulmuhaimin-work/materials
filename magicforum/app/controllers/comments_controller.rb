2398345ac8eba6f3d71c3b0209d085a43f08c322
Abdul Muhaimin <abdulmuhaimin.work@gmail.com>
2016-08-12 15:30:50 +0800

    add AJAX Responder to comment

--- magicforum/app/controllers/comments_controller.rb
+++ magicforum/app/controllers/comments_controller.rb
@@ -1,31 +1,26 @@
 class CommentsController < ApplicationController
 
-  #respond_to :js
-  #before_action :authenticate!, except: [:index]
+  respond_to :js
+  before_action :authenticate!, except: [:index]
 
   def index
     @topic = Topic.includes(:posts).find_by(id: params[:topic_id])
     @post = Post.includes(:comments).find_by(id: params[:post_id])
     @comments = @post.comments
-  end
-
-  def new
-    @topic = Topic.find_by(id: params[:topic_id])
-    @post = Post.find_by(id: params[:post_id])
     @comment = Comment.new
   end
 
   def create
+    @comment = current_user.comments.build(comment_params.merge(post_id: params[:post_id]))
+    @new_comment = Comment.new
     @post = Post.find_by(id: params[:post_id])
     @topic = @post.topic
-    @comment = current_user.comments.build(comment_params.merge(post_id: params[:post_id]))
+
 
     if @comment.save
-      flash[:success] = "You've created a new comment."
-      redirect_to topic_post_comments_path(@topic, @post)
+      flash.now[:success] = "You've created a new comment."
     else
-      flash[:danger] = @comment.errors.full_messages
-      redirect_to new_topic_post_comment_path(@post)
+      flash.now[:danger] = @comment.errors.full_messages
     end
   end
 
